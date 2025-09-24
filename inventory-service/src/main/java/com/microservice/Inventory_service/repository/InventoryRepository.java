6c87155df5c71984a98e5e3446f5d4c70dbbe681
Dhruv Patel <dp14798@gmail.com>
2025-09-24 11:44:07 -0400

    first commit

---
+++ inventory-service/src/main/java/com/microservice/Inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.microservice.Inventory_service.repository;
+
+import com.microservice.Inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
---
+++ product-service/src/main/java/com/microservice/product_service/dto/ProductRequest.java
@@ -0,0 +1,20 @@
+package com.microservice.product_service.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+
+    public String name;
+    public String description;
+    public BigDecimal price;
+}
